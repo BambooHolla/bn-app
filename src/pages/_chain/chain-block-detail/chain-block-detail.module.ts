19359e72462a73ccf8c7bad7c079eed2f0a7cc8b
Gaubee <gaubeebangeel@gmail.com>
2018-01-25 11:48:18 +0800

    实现“区块详情”页面
    Signed-off-by: Gaubee <gaubeebangeel@gmail.com>

--- src/pages/_chain/chain-block-detail/chain-block-detail.module.ts
+++ src/pages/_chain/chain-block-detail/chain-block-detail.module.ts
@@ -1,13 +1,17 @@
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { ChainBlockDetailPage } from './chain-block-detail';
+import { NgModule } from "@angular/core";
+import { IonicPageModule } from "ionic-angular";
+import { ChainBlockDetailPage } from "./chain-block-detail";
+import { ComponentsModule } from "../../../components/components.module";
+import { DirectivesModule } from "../../../directives/directives.module";
+import { MomentModule } from "angular2-moment";
 
 @NgModule({
-  declarations: [
-    ChainBlockDetailPage,
-  ],
-  imports: [
-    IonicPageModule.forChild(ChainBlockDetailPage),
-  ],
+	declarations: [ChainBlockDetailPage],
+	imports: [
+		IonicPageModule.forChild(ChainBlockDetailPage),
+		ComponentsModule,
+		DirectivesModule,
+		MomentModule,
+	],
 })
 export class ChainBlockDetailPageModule {}
--- src/pages/_chain/chain-block-detail/chain-block-detail.ts
+++ src/pages/_chain/chain-block-detail/chain-block-detail.ts
@@ -23,6 +23,40 @@ export class ChainBlockDetailPage extends SecondLevelPage {
 	) {
 		super(navCtrl, navParams, true, tabs);
 	}
+	block_info = {
+		create_time: new Date(
+			Date.now() - Math.random() * 10 * 24 * 60 * 60 * 1000,
+		),
+		address: "b7LA11Tgg3HNiAD6rJMDpD44y3V4WGNX8R",
+		reward: 200 * Math.random(),
+		height: (1000 * Math.random()) | 0,
+		is_delay: Math.random() > 0.5,
+		trans_num: (Math.random() * 5000) | 0,
+		trans_assets: Math.random() * 10000,
+		fee: 5000 * Math.random() * 0.00000001,
+		tran_logs: [],
+	};
+	tran_logs_config = {
+		page: 1,
+		pageSize: 20,
+		has_more: true,
+	};
+	@ChainBlockDetailPage.willEnter
+	async loadTranLogs() {
+		await new Promise(cb => setTimeout(cb, 1000));
+		this.block_info.tran_logs.push(
+			...Array.from(Array(this.tran_logs_config.pageSize)),
+		);
+	}
 
-	
+	async loadMoreTranLogs() {
+		await new Promise(cb => setTimeout(cb, 1000));
+		if (this.block_info.tran_logs.length < 110) {
+			this.block_info.tran_logs.length += this.tran_logs_config.pageSize;
+		}
+		if (this.block_info.tran_logs.length >= 110) {
+			this.block_info.tran_logs.length = 110;
+			this.tran_logs_config.has_more = false;
+		}
+	}
 }
