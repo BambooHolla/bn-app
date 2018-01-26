a3d4aeb4ca1560c29a8c7e23a1ff962cf988bdff
Gaubee <gaubeebangeel@gmail.com>
2018-01-26 11:17:15 +0800

    初步实现了“委托人”页面的样式
    Signed-off-by: Gaubee <gaubeebangeel@gmail.com>

--- src/pages/_vote/vote-delegate-detail/vote-delegate-detail.module.ts
+++ src/pages/_vote/vote-delegate-detail/vote-delegate-detail.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from "@angular/core";
 import { IonicPageModule } from "ionic-angular";
 import { VoteDelegateDetailPage } from "./vote-delegate-detail";
 import { ComponentsModule } from "../../../components/components.module";
+import { DirectivesModule } from "../../../directives/directives.module";
+import { MomentModule } from "angular2-moment";
 
 @NgModule({
 	declarations: [VoteDelegateDetailPage],
 	imports: [
 		IonicPageModule.forChild(VoteDelegateDetailPage),
 		ComponentsModule,
+		DirectivesModule,
+		MomentModule,
 	],
 })
 export class VoteDelegateDetailPageModule {}
--- src/pages/_vote/vote-delegate-detail/vote-delegate-detail.ts
+++ src/pages/_vote/vote-delegate-detail/vote-delegate-detail.ts
@@ -1,7 +1,7 @@
 import { Component, Optional } from "@angular/core";
 import { SecondLevelPage } from "../../../bnqkl-framework/SecondLevelPage";
 import { TabsPage } from "../../tabs/tabs";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { IonicPage, NavController, NavParams, Refresher } from "ionic-angular";
 
 @IonicPage({ name: "vote-delegate-detail" })
 @Component({
@@ -25,5 +25,66 @@ export class VoteDelegateDetailPage extends SecondLevelPage {
 			address: "aFJe90Uwc4SsmKI122fmKI122f90faOKJFES90faOKJFESIOe",
 			get_vote_rate: Math.random() * 100,
 		};
+	}
+
+	block_list: any[];
+	block_list_config = {
+		page: 1,
+		pageSize: 20,
+		has_more: false,
+	};
+	async loadBlockList(refresher?: Refresher) {
+		await new Promise(cb => setTimeout(cb, 1000));
+		const { block_list_config } = this;
+		block_list_config.page = 1;
+		block_list_config.has_more = true;
+
+		this.block_list = Array.from(
+			Array(block_list_config.pageSize),
+		).map((_, i) => {
+			return {
+				create_time: new Date(Date.now() - (i + 1) * 3 * 128 * 1000),
+				address: "b7LA11Tgg3HNiAD6rJMDpD44y3V4WGNX8R",
+				reward: 200 * Math.random(),
+				height: (1000 * Math.random()) | 0,
+				is_delay: Math.random() > 0.5,
+				trans_num: (Math.random() * 5000) | 0,
+				trans_assets: Math.random() * 10000,
+				fee: 5000 * Math.random() * 0.00000001,
+				tran_logs: Array.from(Array(Math.random() * 100)),
+			};
+		});
+		if (refresher) {
+			refresher.complete();
+		}
+	}
+
+	async loadMoreBlockList() {
+		await new Promise(cb => setTimeout(cb, 1000));
+		const { block_list_config } = this;
+		block_list_config.page += 1;
+
+		this.block_list.push(
+			...Array.from(Array(block_list_config.pageSize)).map((_, i) => {
+				return {
+					create_time: new Date(
+						Date.now() - (i + 1) * 3 * 128 * 1000,
+					),
+					address: "b7LA11Tgg3HNiAD6rJMDpD44y3V4WGNX8R",
+					reward: 200 * Math.random(),
+					height: (1000 * Math.random()) | 0,
+					is_delay: Math.random() > 0.5,
+					trans_num: (Math.random() * 5000) | 0,
+					trans_assets: Math.random() * 10000,
+					fee: 5000 * Math.random() * 0.00000001,
+					tran_logs: Array.from(Array(Math.random() * 100)),
+					block_size: Math.random() * 10,
+				};
+			}),
+		);
+		block_list_config.has_more = this.block_list.length < 110;
+		if (!block_list_config.has_more) {
+			this.block_list.length = 110;
+		}
 	}
 }
