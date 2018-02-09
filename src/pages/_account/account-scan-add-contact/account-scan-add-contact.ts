995d82130987f0ede0012103db103ed2d2bb3da9
Gaubee <gaubeebangeel@gmail.com>
2018-02-10 04:59:01 +0800

    临时关闭一些入口按钮
    Signed-off-by: Gaubee <gaubeebangeel@gmail.com>

--- src/pages/_account/account-scan-add-contact/account-scan-add-contact.ts
+++ src/pages/_account/account-scan-add-contact/account-scan-add-contact.ts
@@ -2,10 +2,15 @@ import { ViewChild, ElementRef, Component, Optional } from "@angular/core";
 import { SecondLevelPage } from "../../../bnqkl-framework/SecondLevelPage";
 import { asyncCtrlGenerator } from "../../../bnqkl-framework/Decorator";
 import { TabsPage } from "../../tabs/tabs";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  ViewController,
+} from "ionic-angular";
 // import * as Instascan from "instascan";
 // window["Instascan"] = Instascan;
-import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
+import { QRScanner, QRScannerStatus } from "@ionic-native/qr-scanner";
 import { ContactServiceProvider } from "../../../providers/contact-service/contact-service";
 
 @IonicPage({ name: "account-scan-add-contact" })
@@ -20,12 +25,13 @@ export class AccountScanAddContactPage extends SecondLevelPage {
     @Optional() public tabs: TabsPage,
     public qrScanner: QRScanner,
     public contactService: ContactServiceProvider,
+    public viewCtrl: ViewController,
   ) {
     super(navCtrl, navParams, true, tabs);
     // window["Instascan"] = Instascan;
     // alert(Instascan);
   }
-  titleContent = "扫一扫"
+  titleContent = "扫一扫";
   @AccountScanAddContactPage.willEnter
   initParams() {
     const title = this.navParams.get("title");
@@ -40,76 +46,85 @@ export class AccountScanAddContactPage extends SecondLevelPage {
   @asyncCtrlGenerator.error("扫描异常")
   openCameraMedia() {
     // const scanner = new Instascan.Scanner({
-    // 	video: this.video.nativeElement,
-    // 	mirror: false,
-    // 	captureImage: true,
+    //   video: this.video.nativeElement,
+    //   mirror: false,
+    //   captureImage: true,
     // });
     // return Instascan.Camera.getCameras().then(function(cameras) {
-    // 	if (cameras.length > 0) {
-    // 		console.log(cameras);
-    // 		return scanner.start(cameras[0]);
-    // 	} else {
-    // 		console.error("No cameras found.");
-    // 	}
+    //   if (cameras.length > 0) {
+    //     console.log(cameras);
+    //     return scanner.start(cameras[0]);
+    //   } else {
+    //     console.error("No cameras found.");
+    //   }
     // });
     /**/
-    // if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    //   const video = this.video.nativeElement;
-    //   // Not adding `{ audio: true }` since we only want video now
-    //   navigator.mediaDevices
-    //     .getUserMedia({
-    //       video: {
-    //         width: this.innerWidth,
-    //         height: this.innerHeight,
-    //         facingMode: "environment",
-    //       },
-    //       audio: false,
-    //     })
-    //     .then(function(stream) {
-    //       video.src = window.URL.createObjectURL(stream);
-    //       video.play();
-    //     })
-    //     .catch(err => {
-    //       console.log("qaq", err);
-    //     });
-    // }
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      const video = this.video.nativeElement;
+      // Not adding `{ audio: true }` since we only want video now
+      navigator.mediaDevices
+        .getUserMedia({
+          video: {
+            width: this.innerWidth,
+            height: this.innerHeight,
+            facingMode: "environment",
+          },
+          audio: false,
+        })
+        .then(function(stream) {
+          video.src = window.URL.createObjectURL(stream);
+          video.play();
+        })
+        .catch(err => {
+          console.log("qaq", err);
+        });
+    }
     /**/
-    return this.qrScanner.prepare()
-      .then((status: QRScannerStatus) => {
-        if (status.authorized) {
-          // camera permission was granted
+    // return this.qrScanner.prepare().then((status: QRScannerStatus) => {
+    //   if (status.authorized) {
+    //     // camera permission was granted
 
+    //     // start scanning
+    //     let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+    //       alert("Scanned something："+ text);
 
-          // start scanning
-          let scanSub = this.qrScanner.scan().subscribe((text: string) => {
-            console.log('Scanned something', text);
+    //       this.qrScanner.hide(); // hide camera preview
+    //       scanSub.unsubscribe(); // stop scanning
+    //       const mode = this.navParams.get("mode");
+    //       if (mode === "scan-only") {
+    //         this.jobRes(text);
+    //         this.finishJob();
+    //       } else {
+    //         this.searchContacts();
+    //       }
+    //       this.qrScanner.hide(); // hide camera preview
+    //       scanSub.unsubscribe(); // stop scanning
+    //     });
 
-            this.qrScanner.hide(); // hide camera preview
-            scanSub.unsubscribe(); // stop scanning
-            const mode = this.navParams.get("mode");
-            if (mode === "scan-only") {
-              this.jobRes(text);
-              this.finishJob();
-            } else {
-              this.searchContacts();
-            }
-          });
+    //     this.qrScanner.resumePreview();
+    //     // show camera preview
+    //     this.qrScanner.show().then(
+    //       (data: QRScannerStatus) => {
+    //         alert(data.showing);
+    //       },
+    //       err => {
+    //         alert(err);
+    //       },
+    //     );
 
-          // show camera preview
-          this.qrScanner.show();
-
-          // wait for user to scan something, then the observable callback will be called
-
-        } else if (status.denied) {
-          this.qrScanner.openSettings();
-          // camera permission was permanently denied
-          // you must use QRScanner.openSettings() method to guide the user to the settings page
-          // then they can grant the permission from there
-        } else {
-          // permission was denied, but not permanently. You can ask for permission again at a later time.
-          this.finishJob();
-        }
-      })
+    //     // wait for user to scan something, then the observable callback will be called
+    //   } else if (status.denied) {
+    //     alert("zzzzz");
+    //     this.qrScanner.openSettings();
+    //     // camera permission was permanently denied
+    //     // you must use QRScanner.openSettings() method to guide the user to the settings page
+    //     // then they can grant the permission from there
+    //   } else {
+    //     alert("qaqqqq");
+    //     // permission was denied, but not permanently. You can ask for permission again at a later time.
+    //     this.finishJob();
+    //   }
+    // });
   }
 
   private _ti;
@@ -138,6 +153,6 @@ export class AccountScanAddContactPage extends SecondLevelPage {
       address,
       pay_pwd,
     );
-    this.finishJob()
+    this.finishJob();
   }
 }
