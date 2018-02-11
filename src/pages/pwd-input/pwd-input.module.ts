556255c778fb3456b927e4a70f556c1f601593db
Gaubee <gaubeebangeel@gmail.com>
2018-02-11 23:23:30 +0800

    add byte.pipe
    Signed-off-by: Gaubee <gaubeebangeel@gmail.com>

--- src/pages/pwd-input/pwd-input.module.ts
+++ src/pages/pwd-input/pwd-input.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from "@angular/core";
 import { IonicPageModule } from "ionic-angular";
 import { PwdInputPage } from "./pwd-input";
 import { DirectivesModule } from "../../directives/directives.module";
+import { TranslateModule } from "@ngx-translate/core";
 
 @NgModule({
 	declarations: [PwdInputPage],
-	imports: [IonicPageModule.forChild(PwdInputPage), DirectivesModule],
+	imports: [
+		IonicPageModule.forChild(PwdInputPage),
+		DirectivesModule,
+		TranslateModule,
+	],
 })
 export class PwdInputPageModule {}
--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -3,9 +3,22 @@ import { MaskPipe } from "./mask/mask";
 import { TimestampPipe } from "./timestamp/timestamp";
 import { DatediffPipe } from "./datediff/datediff";
 import { AmountEulerPipe } from "./amount-euler/amount-euler";
+import { BytesPipe } from "./bytes/bytes";
 @NgModule({
-  declarations: [MaskPipe, TimestampPipe, DatediffPipe, AmountEulerPipe],
-  imports: [],
-  exports: [MaskPipe, TimestampPipe, DatediffPipe, AmountEulerPipe],
+	declarations: [
+		MaskPipe,
+		TimestampPipe,
+		DatediffPipe,
+		AmountEulerPipe,
+		BytesPipe,
+	],
+	imports: [],
+	exports: [
+		MaskPipe,
+		TimestampPipe,
+		DatediffPipe,
+		AmountEulerPipe,
+		BytesPipe,
+	],
 })
 export class PipesModule {}
