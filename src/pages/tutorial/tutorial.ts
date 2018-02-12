7aff3010c9432cd81f366d09d0a25f0407340fb9
Gaubee <gaubeebangeel@gmail.com>
2018-02-12 11:09:30 +0800

    增加启动页的样式与素材
    Signed-off-by: Gaubee <gaubeebangeel@gmail.com>

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -48,17 +48,17 @@ export class TutorialPage {
           {
             title: values.TUTORIAL_SLIDE1_TITLE,
             description: values.TUTORIAL_SLIDE1_DESCRIPTION,
-            image: "assets/img/ica-slidebox-img-1.png",
+            image: "assets/imgs/tutorial/1.jpg",
           },
           {
             title: values.TUTORIAL_SLIDE2_TITLE,
             description: values.TUTORIAL_SLIDE2_DESCRIPTION,
-            image: "assets/img/ica-slidebox-img-2.png",
+            image: "assets/imgs/tutorial/2.jpg",
           },
           {
             title: values.TUTORIAL_SLIDE3_TITLE,
             description: values.TUTORIAL_SLIDE3_DESCRIPTION,
-            image: "assets/img/ica-slidebox-img-3.png",
+            image: "assets/imgs/tutorial/3.jpg",
           },
         ];
       });
