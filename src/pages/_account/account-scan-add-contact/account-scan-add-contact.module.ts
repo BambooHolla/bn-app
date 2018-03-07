0c18a56c6728aa390ac73c9d6040a73a39510ceb
Gaubee <gaubeebangeel@gmail.com>
2018-03-07 18:08:39 +0800

    f Signed-off-by: Gaubee <gaubeebangeel@gmail.com>

--- src/pages/_account/account-scan-add-contact/account-scan-add-contact.module.ts
+++ src/pages/_account/account-scan-add-contact/account-scan-add-contact.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from "@angular/core";
 import { IonicPageModule } from "ionic-angular";
 import { AccountScanAddContactPage } from "./account-scan-add-contact";
 import { TranslateModule } from "@ngx-translate/core";
+import { ComponentsModule } from "../../../components/components.module";
 
 @NgModule({
   declarations: [AccountScanAddContactPage],
   imports: [
     IonicPageModule.forChild(AccountScanAddContactPage),
     TranslateModule,
+    ComponentsModule,
   ],
 })
 export class AccountScanAddContactPageModule {}
