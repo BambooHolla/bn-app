c85275e7f36add9150a181ad2c880ea975eb0fd9
Gaubee <gaubeebangeel@gmail.com>
2018-03-14 11:23:55 +0800

    改进可复制性，包括以下页面： 交易明细页 添加联系人页 转账回执页 区块信息页 我的
    Signed-off-by: Gaubee <gaubeebangeel@gmail.com>

--- src/pages/_account/account-add-contact/account-add-contact.module.ts
+++ src/pages/_account/account-add-contact/account-add-contact.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { IonicPageModule } from "ionic-angular";
 import { AccountAddContactPage } from "./account-add-contact";
 import { ComponentsModule } from "../../../components/components.module";
+import { DirectivesModule } from "../../../directives/directives.module";
 import { TranslateModule } from "@ngx-translate/core";
 
 @NgModule({
@@ -9,6 +10,7 @@ import { TranslateModule } from "@ngx-translate/core";
   imports: [
     IonicPageModule.forChild(AccountAddContactPage),
     ComponentsModule,
+    DirectivesModule,
     TranslateModule,
   ],
 })
--- src/pages/_chain/chain-transaction-detail/chain-transaction-detail.module.ts
+++ src/pages/_chain/chain-transaction-detail/chain-transaction-detail.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { IonicPageModule } from "ionic-angular";
 import { ChainTransactionDetailPage } from "./chain-transaction-detail";
 import { ComponentsModule } from "../../../components/components.module";
+import { DirectivesModule } from "../../../directives/directives.module";
 import { PipesModule } from "../../../pipes/pipes.module";
 import { MomentModule } from "angular2-moment";
 import { TranslateModule } from "@ngx-translate/core";
@@ -11,6 +12,7 @@ import { TranslateModule } from "@ngx-translate/core";
   imports: [
     IonicPageModule.forChild(ChainTransactionDetailPage),
     ComponentsModule,
+    DirectivesModule,
     PipesModule,
     MomentModule,
     TranslateModule,
