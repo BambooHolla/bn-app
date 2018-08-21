02dc3e7a8123e904425ee96e4c83dbd06720d09e
Gaubee <gaubeebangeel@gmail.com>
2018-08-21 22:02:52 +0800

    完善了saveTag的实现
    Signed-off-by: Gaubee <gaubeebangeel@gmail.com>

--- src/pages/_account/account-tag-detail/account-tag-detail.ts
+++ src/pages/_account/account-tag-detail/account-tag-detail.ts
@@ -57,6 +57,8 @@ export class AccountTagDetailPage extends SecondLevelPage {
 	/**未修改的情况下原本的的联系人列表*/
 	private _source_contact_list: LocalContactModel[] = [];
 
+	private _load_local_contact_list?: Promise<void>;
+
 	@AccountTagDetailPage.willEnter
 	initData() {
 		const tag_info: TagModel = this.navParams.get("tag");
@@ -68,7 +70,7 @@ export class AccountTagDetailPage extends SecondLevelPage {
 		this.is_create_mode = !tag_info;
 		this.formData.tag_name = tag_info.name;
 
-		this._loadLocalContactList();
+		this._load_local_contact_list = this._loadLocalContactList();
 	}
 
 	@asyncCtrlGenerator.error()
@@ -111,21 +113,46 @@ export class AccountTagDetailPage extends SecondLevelPage {
 	@asyncCtrlGenerator.error()
 	@asyncCtrlGenerator.success("@@TAG_SAVE_SUCCESS")
 	async saveTagDetail() {
+		let tag_id = this.tag_info._id;
+		const contact_list = this.contact_list.slice();
 		if (this.is_create_mode) {
+			await this._load_local_contact_list; // 一定要确保联系人数据加载完了，不然没法移除已有的联系人的标签
 			const tagName = this.formData.tag_name;
-			const contact_list = this.contact_list.slice();
-			const new_tag_id = await this.localContact.addTag(
-				tagName,
-				contact_list.map(c => c._id)
+			tag_id = await this.localContact.addTag(
+				(this.tag_info.name = tagName),
+				(this.tag_info.contact_ids = contact_list.map(c => c._id))
 			);
 			contact_list.map(contact => {
 				contact.tags.push(tagName);
 			});
 		} else {
 			this.tag_info.name = this.formData.tag_name;
-			this.tag_info.contact_ids = this.contact_list.map(c => c._id);
+			this.tag_info.contact_ids = contact_list.map(c => c._id);
 			await this.localContact.updateTag(this.tag_info);
 		}
+		/// 更新相关联系人的标签数据
+		// 目前标签已有的ids
+		const contact_ids_set = new Map<string, LocalContactModel>();
+		contact_list.forEach(c => contact_ids_set.set(c._id, c));
+		// 排除已经不再ids中的，并修改他们
+		await Promise.all(
+			this._source_contact_list.map(async contact => {
+				contact_ids_set.delete(contact._id);
+				if (!contact_ids_set.has(contact._id)) {
+					contact.tags = contact.tags.filter(t_id => t_id !== tag_id);
+					return this.localContact.updateLocaContact(contact);
+				}
+			})
+		);
+		/// 剩余的就是新增的联系人的了，修改他们
+		for (var _c of contact_ids_set.values()) {
+			const contact = _c;
+			if (contact.tags.indexOf(tag_id) === -1 /*一个无可厚非的去重*/) {
+				contact.tags.push(tag_id);
+				await this.localContact.updateLocaContact(contact);
+			}
+		}
+
 		this.finishJob();
 	}
 
