733a54d1d607cb1e9f553e6f086bffba0e62f373
Gaubee <gaubeebangeel@gmail.com>
2018-10-11 22:19:19 +0800

    修复ripple回滚动画
    Signed-off-by: Gaubee <gaubeebangeel@gmail.com>

--- src/app/ripple.transition.ts
+++ src/app/ripple.transition.ts
@@ -2,17 +2,22 @@ import { Animation } from "ionic-angular/animations/animation";
 import { isPresent } from "ionic-angular/util/util";
 import { PageTransition } from "ionic-angular/transitions/page-transition";
 import { ViewController } from "ionic-angular/navigation/view-controller";
+import { NavController } from "ionic-angular/navigation/nav-controller";
 import { ElementRef } from "@angular/core";
 
 const DURATION = 500;
 const CLIP_PATH = "-webkit-clip-path";
 const SHOW_BACK_BTN_CSS = "show-back-button";
-const POP_SYMBOL = Symbol.for("nav.pop");
+const POP_SYMBOL = Symbol("RippleTransition.nav.pop");
+const BACK_ANI_SYMBOL = Symbol("RippleTransition.backAni");
+const OPACITY = "opacity";
+const TRANSPARENT = 0;
+const OPAQUE = 1;
 
 function getTouchPosAndCircleR(view: ViewController) {
   const W = document.body.clientWidth;
   const H = document.body.clientHeight;
-  const navParams = view.getNavParams()
+  const navParams = view.getNavParams();
   const touchPos: {
     x: number;
     y: number;
@@ -29,8 +34,8 @@ function getTouchPosAndCircleR(view: ViewController) {
   return {
     touchPos,
     circleR: R,
-    isBackAni: navParams.get("register_back_animation")
-  }
+    isBackAni: navParams.get("register_back_animation"),
+  };
 }
 
 export class RippleTransition extends PageTransition {
@@ -48,13 +53,10 @@ export class RippleTransition extends PageTransition {
     const backDirection = opts.direction === "back";
 
     if (enteringView) {
-
       const enteringPageEle: HTMLElement = enteringView.pageRef().nativeElement;
 
       const enteringContent = new Animation(this.plt, enteringPageEle);
-      const enteringBackButton = new Animation(this.plt,
-        enteringPageEle.querySelector("ion-navbar .back-button")
-      );
+      const enteringBackButton = new Animation(this.plt, enteringPageEle.querySelector("ion-navbar .back-button"));
       this.add(enteringContent);
       this.add(enteringBackButton);
       enteringBackButton.beforeAddClass(SHOW_BACK_BTN_CSS);
@@ -65,21 +67,31 @@ export class RippleTransition extends PageTransition {
 
         const { touchPos, circleR, isBackAni } = getTouchPosAndCircleR(enteringView);
         enteringContent.fromTo(CLIP_PATH, `circle(0% at ${touchPos.x}px ${touchPos.y}px)`, `circle(${circleR}px at ${touchPos.x}px ${touchPos.y}px)`, true);
+        enteringContent.fromTo(OPACITY, 0.4, OPAQUE, true);
 
         // 改写pop
-        if (!enteringView._nav[POP_SYMBOL]) {
-          enteringView._nav[POP_SYMBOL] = enteringView._nav.pop;
+        const nav = enteringView._nav;
+        let need_back_ani_views: Set<ViewController> = nav[BACK_ANI_SYMBOL];
+        if (!nav[POP_SYMBOL]) {
+          nav[POP_SYMBOL] = nav.pop;
+          need_back_ani_views = nav[BACK_ANI_SYMBOL] = new Set<ViewController>();
+          nav.pop = function(opts?, done?) {
+            const leavingView = nav.getActive();
+            if (need_back_ani_views.has(leavingView)) {
+              need_back_ani_views.delete(leavingView);
+              opts = Object.assign(
+                {
+                  animation: "ripple-transition",
+                },
+                opts
+              );
+            }
+            return nav[POP_SYMBOL](opts, done);
+          };
         }
+        enteringView[BACK_ANI_SYMBOL] = isBackAni;
         if (isBackAni) {
-          enteringView._nav.pop = (opts?, done?) => {
-            opts = Object.assign({
-              animation: "ripple-transition",
-              duration: this.getDuration(),
-            }, opts);
-            // 一次性动画注册
-            enteringView._nav.pop = enteringView._nav[POP_SYMBOL];
-            return enteringView._nav.pop(opts, done);
-          }
+          need_back_ani_views.add(enteringView);
         }
       }
     }
@@ -88,9 +100,7 @@ export class RippleTransition extends PageTransition {
       const leavingPageEle: HTMLElement = leavingView.pageRef().nativeElement;
 
       const leavingContent = new Animation(this.plt, leavingPageEle);
-      const leavingBackButton = new Animation(this.plt,
-        leavingPageEle.querySelector("ion-navbar .back-button")
-      );
+      const leavingBackButton = new Animation(this.plt, leavingPageEle.querySelector("ion-navbar .back-button"));
       this.add(leavingContent);
       this.add(leavingBackButton);
       leavingBackButton.beforeRemoveClass(SHOW_BACK_BTN_CSS);
@@ -98,9 +108,10 @@ export class RippleTransition extends PageTransition {
       if (backDirection) {
         // 返回模式
 
-        const { touchPos, circleR } = getTouchPosAndCircleR(enteringView);
+        const { touchPos, circleR } = getTouchPosAndCircleR(leavingView);
 
         leavingContent.fromTo(CLIP_PATH, `circle(${circleR}px at ${touchPos.x}px ${touchPos.y}px)`, `circle(0% at ${touchPos.x}px ${touchPos.y}px)`, true);
+        leavingContent.fromTo(OPACITY, OPAQUE, 0.4, true);
       }
     }
   }
