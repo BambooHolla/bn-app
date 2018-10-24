cf0de24426226f9c1da5710ef393a7749e69603b
Gaubee <kezhaofeng@bnqkl.cn>
2018-10-24 20:34:44 +0800

    账户信息中，用户第一次点击设置用户名时提示

--- src/pages/_account/account-info/account-info.module.ts
+++ src/pages/_account/account-info/account-info.module.ts
@@ -4,14 +4,10 @@ import { AccountInfoPage } from "./account-info";
 import { ComponentsModule } from "../../../components/components.module";
 import { DirectivesModule } from "../../../directives/directives.module";
 import { TranslateModule } from "@ngx-translate/core";
+import { MatButtonModule } from "@angular/material";
 
 @NgModule({
-  declarations: [AccountInfoPage],
-  imports: [
-    IonicPageModule.forChild(AccountInfoPage),
-    ComponentsModule,
-    DirectivesModule,
-    TranslateModule,
-  ],
+	declarations: [AccountInfoPage],
+	imports: [IonicPageModule.forChild(AccountInfoPage), ComponentsModule, DirectivesModule, TranslateModule, MatButtonModule],
 })
 export class AccountInfoPageModule {}
--- src/pages/_account/account-info/account-info.ts
+++ src/pages/_account/account-info/account-info.ts
@@ -1,12 +1,7 @@
 import { Component, Optional, ViewChild } from "@angular/core";
 import { SecondLevelPage } from "../../../bnqkl-framework/SecondLevelPage";
 import { TabsPage } from "../../tabs/tabs";
-import {
-  IonicPage,
-  NavController,
-  NavParams,
-  ViewController,
-} from "ionic-angular/index";
+import { IonicPage, NavController, NavParams, ViewController } from "ionic-angular/index";
 // import { KjuaQrcodeComponent } from "../../../components/kjua-qrcode/kjua-qrcode";
 
 @IonicPage({ name: "account-info" })
@@ -15,13 +10,13 @@ import {
   templateUrl: "account-info.html",
 })
 export class AccountInfoPage extends SecondLevelPage {
-  constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    @Optional() public tabs: TabsPage,
-    public viewCtrl: ViewController
-  ) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, @Optional() public tabs: TabsPage, public viewCtrl: ViewController) {
     super(navCtrl, navParams, true, tabs);
   }
   // @ViewChild(KjuaQrcodeComponent) qrcode!: KjuaQrcodeComponent;
+  async routeToSetUsername() {
+    if (await this.waitTipDialogConfirm("@@SET_USERNAME_TIP")) {
+      await this.routeTo("account-set-username", { auto_return: true });
+    }
+  }
 }
