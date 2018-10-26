3d436d4a5735ce3a7a582efe77cbc53794feaa9e
Gaubee <kezhaofeng@bnqkl.cn>
2018-10-26 10:11:11 +0800

    IBT手机端区块链列表中，超过一个小时的区块创建时间一律显示原始时间，只有1个小时内的才显示几分钟前。

--- src/pipes/common-duration-humainze/common-duration-humainze.ts
+++ src/pipes/common-duration-humainze/common-duration-humainze.ts
@@ -1,26 +1,29 @@
 import { Pipe, PipeTransform } from "@angular/core";
 import * as moment from "moment";
+import { FLP_Tool } from "../../bnqkl-framework/FLP_Tool";
+import { TranslateService } from "@ngx-translate/core";
 
 @Pipe({
   name: "commonDurationHumainze",
 })
 export class CommonDurationHumainzePipe implements PipeTransform {
+  @FLP_Tool.FromGlobal translate!: TranslateService;
+  get localName() {
+    return FLP_Tool.formatLocalName(this.translate.currentLang);
+  }
   /**
    * 一小时以内，显示人性化的，一小时以后，显示具体时间
    */
-  transform(date, lang, format = "YYYY.MM.DD hh:mm") {
+  transform(date, lang = this.localName, format = "YYYY.MM.DD hh:mm") {
     const now = Date.now();
     const date_num = +date;
     const diff = now - date_num;
     if (diff > 3600000 /* 1h */) {
-      return moment(date_num)
-        .locale(lang)
-        .format(format);
+      const m = moment(date_num).locale(lang);
+      return m.format(format);
     } else {
-      return moment
-        .duration(diff / 1000, "seconds")
-        .locale(lang)
-        .humanize(lang);
+      const m = moment.duration(diff / 1000, "seconds").locale(lang);
+      return m.humanize(true);
     }
   }
 }
